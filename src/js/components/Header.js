561c32ba524109d0861507790f5d89b082653a6d
sindrebenjamin <sindskog@gmail.com>
2023-11-25 10:16:13 +0100

    clean up code in index.js, move functions to Header.js instead

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,8 +2,9 @@ import { Logo } from "./Logo.js";
 import { Nav } from "./Nav.js";
 import { Searchbar } from "./Searchbar.js";
 import { Searchbutton } from "./Searchbutton.js";
+import { logout } from "../api/auth/logout.js";
 
-export const Header = () => {
+const Header = () => {
   return `<div class="m-auto max-w-[1200px] items-center flex justify-between w-full">
   <div class="mr-4">${Logo("fill-primary-400")} 
   </div>
@@ -13,4 +14,57 @@ export const Header = () => {
   ${Nav()}
   </div>
   </div>`;
+};
+
+export const setHeader = () => {
+  const headerElement = document.createElement("header");
+  headerElement.innerHTML = Header();
+  headerElement.classList.add("bg-white", "py-3", "px-4");
+
+  document.body.prepend(headerElement);
+
+  // Toggle NavMenu visibility
+
+  const headerProfile = document.querySelector("#header-profile");
+  const navMenu = document.querySelector("#nav-menu");
+
+  headerProfile.onclick = () => {
+    navMenu.classList.toggle("hidden");
+    headerProfile.classList.toggle("opacity-50");
+  };
+
+  document.onclick = (event) => {
+    const isClickInsideMenu = navMenu.contains(event.target);
+    const isClickInsideAvatar = headerProfile.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickInsideAvatar) {
+      navMenu.classList.add("hidden");
+      headerProfile.classList.remove("opacity-50");
+    }
+  };
+
+  // Logout
+
+  const logoutBtn = document.querySelector("#logout-btn");
+  logoutBtn.onclick = () => logout();
+
+  // Toggle searchbar visibility
+
+  const searchForm = document.querySelector("#search-form");
+  const searchBtn = document.querySelector("#search-btn");
+  const searchBar = document.querySelector("#search");
+  const nav = document.querySelector("nav");
+
+  searchBtn.onclick = () => {
+    nav.classList.add("hidden");
+    searchForm.classList.remove("hidden");
+    searchBtn.classList.add("hidden");
+    searchBar.focus();
+  };
+
+  searchBar.onblur = () => {
+    nav.classList.remove("hidden");
+    searchForm.classList.add("hidden");
+    searchBtn.classList.remove("hidden");
+  };
 };
--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,55 +1,18 @@
-import { Header } from "./Header.js";
-import { logout } from "../api/auth/logout.js";
+import { setHeader } from "./Header.js";
+
 import { addClasses } from "../utils/addClasses.js";
 
 // Header
 
-const headerElement = document.createElement("header");
-addClasses(headerElement, ["bg-white", "py-3", "px-4"]);
-headerElement.innerHTML = Header();
-document.body.prepend(headerElement);
+setHeader();
 
-// Toggle NavMenu visibility
+/*
 
-const headerProfile = document.querySelector("#header-profile");
-const navMenu = document.querySelector("#nav-menu");
 
-headerProfile.onclick = () => {
-  navMenu.classList.toggle("hidden");
-  headerProfile.classList.toggle("opacity-50");
-};
 
-document.onclick = (event) => {
-  const isClickInsideMenu = navMenu.contains(event.target);
-  const isClickInsideAvatar = headerProfile.contains(event.target);
 
-  if (!isClickInsideMenu && !isClickInsideAvatar) {
-    navMenu.classList.add("hidden");
-    headerProfile.classList.remove("opacity-50");
-  }
-};
 
-// Logout
 
-const logoutBtn = document.querySelector("#logout-btn");
-logoutBtn.onclick = () => logout();
 
-// Toggle searchbar visibility
 
-const searchForm = document.querySelector("#search-form");
-const searchBtn = document.querySelector("#search-btn");
-const searchBar = document.querySelector("#search");
-const nav = document.querySelector("nav");
-
-searchBtn.onclick = () => {
-  nav.classList.add("hidden");
-  searchForm.classList.remove("hidden");
-  searchBtn.classList.add("hidden");
-  searchBar.focus();
-};
-
-searchBar.onblur = () => {
-  nav.classList.remove("hidden");
-  searchForm.classList.add("hidden");
-  searchBtn.classList.remove("hidden");
-};
+*/
