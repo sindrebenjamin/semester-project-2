31423238ab695d629addf0da897db9808769b20b
sindrebenjamin <sindskog@gmail.com>
2023-11-26 19:59:08 +0100

    add sort function for browse page

--- src/js/pages/browse.js
+++ src/js/pages/browse.js
@@ -9,12 +9,9 @@ window.onscroll = () => {
   console.log(offset);
   if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
     offset = offset + 100;
-
     getListings("#browse-listings", `${sortString}&limit=100&offset=${offset}`);
   }
 };
-
-//sortString = `&sort=created&sortOrder=desc`;
 
 const sortSelect = document.querySelector("#sort-select");
 const sortMenu = document.querySelector("#sort-menu");
@@ -24,7 +21,7 @@ const current = document.querySelector("#sort-current");
 const listingsContainer = document.querySelector("#browse-listings");
 let currentText = "Ending soon";
 
-sortNewest.onclick = () => {
+const setNewest = () => {
   if (currentText === "Ending soon") {
     currentText = "Newest";
     current.innerText = currentText;
@@ -33,14 +30,12 @@ sortNewest.onclick = () => {
     sortNewest.classList.remove("border-neutral-200");
     sortNewest.classList.add("border-primary-200");
     listingsContainer.innerHTML = "";
-    getListings(
-      "#browse-listings",
-      `&sort=created&sortOrder=desc&limit=100&offset=${offset}`
-    );
+    sortString = `&sort=created&sortOrder=desc`;
+    getListings("#browse-listings", `${sortString}&limit=100`);
   }
 };
 
-sortEnding.onclick = () => {
+const setEnding = () => {
   if (currentText === "Newest") {
     currentText = "Ending soon";
     current.innerText = currentText;
@@ -49,9 +44,21 @@ sortEnding.onclick = () => {
     sortEnding.classList.remove("border-neutral-200");
     sortEnding.classList.add("border-primary-200");
     listingsContainer.innerHTML = "";
-    getListings(
-      "#browse-listings",
-      `&sort=created&sortOrder=desc&limit=100&offset=${offset}`
-    );
+    sortString = `&sort=endsAt&sortOrder=asc`;
+    getListings("#browse-listings", `${sortString}&limit=100`);
   }
 };
+
+sortNewest.onclick = () => {
+  setNewest();
+};
+
+sortEnding.onclick = () => {
+  setEnding();
+};
+
+const queryOrder = new URLSearchParams(window.location.search).get("order");
+
+if (queryOrder === "newest") {
+  setNewest();
+}
