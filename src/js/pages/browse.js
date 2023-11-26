c6e77107a80bb7e67a4f96409b0b2d4ba270070f
sindrebenjamin <sindskog@gmail.com>
2023-11-26 13:09:43 +0100

    infinite scroll on browse page

--- src/js/pages/browse.js
+++ src/js/pages/browse.js
@@ -1,3 +1,22 @@
 import { getListings } from "../api/listings/getListings.js";
 
-getListings;
+getListings(
+  "#browse-listings",
+  false,
+  "&sort=created&sortOrder=desc&limit=100"
+);
+
+let offset = 0;
+
+window.onscroll = () => {
+  console.log(offset);
+  if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+    offset = offset + 100;
+
+    getListings(
+      "#browse-listings",
+      false,
+      `&sort=created&sortOrder=desc&limit=100&offset=${offset}`
+    );
+  }
+};
