de4aa169baed1cacf309b5f6453b8ea363bc1712
sindrebenjamin <sindskog@gmail.com>
2023-11-27 13:21:41 +0100

    improve safety of card component

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -6,8 +6,30 @@ import { checkTitle } from "../utils/checkTitle.js";
 export const Card = async (listing) => {
   const media = await checkMedia(listing.media[0]);
   const daysLeft = dateToDays(listing.endsAt);
-  const bid = currentBid(listing.bids);
-  const title = checkTitle(listing.title);
+  const bid = document.createElement("p");
+  bid.innerText = currentBid(listing.bids);
+  bid.classList.add("font-bold");
+
+  const title = document.createElement("h3");
+  title.innerText = checkTitle(listing.title);
+  title.classList.add("text-secondary-800", "text-xl", "font-bold");
+
+  return `<a class="hover:opacity-50 transition-all flex flex-col gap-4" href="listing.html?id=${listing.id}">
+  <img
+    class="object-cover w-full h-full aspect-square"
+    src="${media}"
+    alt=""
+  />
+
+${title.outerHTML}
+<div>
+  <p>Current bid</p>
+  ${bid.outerHTML}
+</div>
+<p class="text-secondary-100">${daysLeft}</p>
+</a>`;
+
+  /*
 
   return `<a class="hover:opacity-50 transition-all flex flex-col gap-4" href="listing.html?id=${listing.id}">
 
@@ -25,4 +47,7 @@ export const Card = async (listing) => {
     </div>
     <p class="text-secondary-100">${daysLeft}</p>
   </a>`;
+
+
+  */
 };
--- src/js/utils/currentBid.js
+++ src/js/utils/currentBid.js
@@ -1,11 +1,11 @@
 export const currentBid = (bids) => {
   if (bids.length === 0) {
-    return "0";
+    return "No bids yet";
   }
   const sorted = bids.sort((a, b) => a - b);
   const highestBid = sorted.length - 1;
 
   if (sorted[highestBid].amount > 0) {
-    return sorted[highestBid].amount;
+    return "$" + sorted[highestBid].amount;
   }
 };
