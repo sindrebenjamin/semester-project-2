1a4fe8e3a827eb71a58901cb676cc5d4372b0d38
sindrebenjamin <sindskog@gmail.com>
2023-11-29 14:36:54 +0100

    display message when no results or no more listings

--- src/js/api/listings/getListings.js
+++ src/js/api/listings/getListings.js
@@ -3,6 +3,7 @@ import { apiURL } from "../constants.js";
 import { printListings } from "./printListings.js";
 import { checkErrors } from "../checkErrors.js";
 import { displayErrors } from "../displayErrors.js";
+const search = new URLSearchParams(window.location.search).get("search");
 
 export async function getListings(contentHolder, queryString) {
   const options = {
@@ -16,13 +17,17 @@ export async function getListings(contentHolder, queryString) {
       options
     );
     const result = await response.json();
+    let endReached = false;
 
     checkErrors(result);
     if (response.ok) {
       printListings(result, contentHolder);
-      if (result.length === 0) {
-        const holder = document.querySelector(contentHolder);
-        holder.innerHTML = "No results";
+      if (search && result.length === 0) {
+        document.querySelector(contentHolder).innerHTML = "No results";
+      } else if (result.length === 0 && !endReached) {
+        endReached = true;
+        document.querySelector(contentHolder).innerHTML +=
+          "You have reached the end";
       }
     }
   } catch (e) {
