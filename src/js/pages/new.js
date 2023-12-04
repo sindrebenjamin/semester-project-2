eb7c9fca36f40091c4db72e0d7b125ea611edfe3
sindrebenjamin <sindskog@gmail.com>
2023-12-04 15:01:48 +0100

    make it clear when required information is missing for new listing form

--- src/js/pages/new.js
+++ src/js/pages/new.js
@@ -5,6 +5,7 @@ import { captureTags } from "../listeners/forms/inputTags.js";
 import { captureImages } from "../listeners/forms/inputImages.js";
 import { dateToISO } from "../utils/dateToIso.js";
 import { postListing } from "../api/listings/postListing.js";
+import { setError } from "../listeners/forms/setError.js";
 
 inputImages("Add up to 8 photos");
 inputDate();
@@ -15,6 +16,9 @@ const listingForm = document.querySelector("#listing-form");
 listingForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const data = new FormData(e.target);
+  const dateTest = testDate();
+  const titleTest = testTitle();
+
   const body = {
     title: data.get("title"),
     description: data.get("description"),
@@ -23,5 +27,25 @@ listingForm.addEventListener("submit", function (e) {
     endsAt: dateToISO(data.get("end-date")),
   };
 
-  postListing(body, "POST");
+  if (dateTest && titleTest) {
+    postListing(body, "POST");
+  }
 });
+
+const testDate = () => {
+  const input = document.querySelector("#end-date");
+  const label = document.querySelector("#end-date-label");
+  const hint = document.querySelector("#end-date-hint");
+  const test = !input.value ? false : true;
+  setError(test, input, hint, label, "Please choose an end date");
+  return test;
+};
+
+const testTitle = () => {
+  const input = document.querySelector("#title");
+  const label = document.querySelector("#title-label");
+  const hint = document.querySelector("#title-hint");
+  const test = !input.value ? false : true;
+  setError(test, input, hint, label, "Must include title");
+  return test;
+};
