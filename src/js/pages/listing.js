b9cce2f54d37a605ff5eda0d2cc1be4134c700a1
sindrebenjamin <sindskog@gmail.com>
2023-12-05 13:56:29 +0100

    create basic carousel functionality, possible to change picture

--- src/js/pages/listing.js
+++ src/js/pages/listing.js
@@ -7,3 +7,31 @@ async function getData() {
 }
 
 getData();
+
+const images = document.querySelectorAll(".carousel-img");
+
+let currentIndex = 0;
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    image.style.transform = `translateX(${100 * (i - index)}%)`;
+  });
+}
+
+function next() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prev() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    next();
+  } else if (e.key === "ArrowLeft") {
+    prev();
+  }
+});
