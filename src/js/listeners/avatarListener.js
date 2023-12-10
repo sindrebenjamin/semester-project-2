2778fe69b0f63a76d824a66b657e31d80d06b2cd
sindrebenjamin <sindskog@gmail.com>
2023-12-10 19:21:35 +0100

    create avatar indicator for register form

--- src/js/listeners/avatarListener.js
+++ src/js/listeners/avatarListener.js
@@ -6,88 +6,86 @@ import { setError } from "./forms/setError.js";
 
 const profile = load("profile");
 
-if (profile) {
-  const profileAvatar = checkAvatar(profile.avatar);
-  const currentAvatar = document.querySelector("#current-avatar");
-  const settingsForm = document.querySelector("#settings-form");
-  const inputField = document.querySelector("#avatar");
-  const discardBtn = document.querySelector("#discard-btn");
-  const settingsWrapper = document.querySelector("#settings-wrapper");
-  const profileSection = document.querySelector("#profile");
+const profileAvatar = checkAvatar(profile.avatar);
+const currentAvatar = document.querySelector("#current-avatar");
+const settingsForm = document.querySelector("#settings-form");
+const inputField = document.querySelector("#avatar");
+const discardBtn = document.querySelector("#discard-btn");
+const settingsWrapper = document.querySelector("#settings-wrapper");
+const profileSection = document.querySelector("#profile");
 
+currentAvatar.src = profileAvatar;
+profile.avatar && (inputField.value = profileAvatar);
+
+let testResult = true;
+
+inputAvatarListener("", testing);
+function testing(test, input) {
+  testResult = test;
+  // Update avatar preview oninput
+  if (test && input.value) {
+    currentAvatar.src = input.value;
+  } else if (input.value === "") {
+    currentAvatar.src = "./public/noavatar.png";
+  }
+}
+
+// PUT request
+
+settingsForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const body = {
+    avatar: inputField.value,
+  };
+  testResult && putAvatar(body);
+});
+
+// Discard changes
+
+discardBtn.onclick = (e) => {
+  e.preventDefault();
+  discard();
+};
+
+function discard() {
+  inputField.value = profile.avatar ? profileAvatar : "";
   currentAvatar.src = profileAvatar;
-  profile.avatar && (inputField.value = profileAvatar);
+  settingsWrapper.classList.add("hidden");
+  profileSection.classList.remove("hidden");
+}
 
-  let testResult = true;
+currentAvatar.onclick = () => {
+  inputField.focus();
+};
 
-  inputAvatarListener("", testing);
-  function testing(test, input) {
-    testResult = test;
-    // Update avatar preview oninput
-    if (test && input.value) {
-      currentAvatar.src = input.value;
-    } else if (input.value === "") {
-      currentAvatar.src = "./public/noavatar.png";
-    }
-  }
+// Settings visibility
 
-  // PUT request
+const label = document.querySelector("#avatar-label");
+const hint = document.querySelector("#avatar-hint");
 
-  settingsForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const body = {
-      avatar: inputField.value,
-    };
-    testResult && putAvatar(body);
-  });
-
-  // Discard changes
-
-  discardBtn.onclick = (e) => {
-    e.preventDefault();
-    discard();
-  };
-
-  function discard() {
-    inputField.value = profile.avatar ? profileAvatar : "";
-    currentAvatar.src = profileAvatar;
-    settingsWrapper.classList.add("hidden");
-    profileSection.classList.remove("hidden");
-  }
-
-  currentAvatar.onclick = () => {
+document.addEventListener("click", (e) => {
+  if (
+    e.target === "change-avatar" ||
+    e.target.closest("#change-avatar") ||
+    e.target === "avatar-img" ||
+    e.target.closest("#avatar-img")
+  ) {
+    testResult = true;
+    setError(testResult, inputField, hint, label);
+    settingsWrapper.classList.remove("hidden");
+    profileSection.classList.add("hidden");
     inputField.focus();
-  };
+  }
 
-  // Settings visibility
-
-  const label = document.querySelector("#avatar-label");
-  const hint = document.querySelector("#avatar-hint");
-
-  document.addEventListener("click", (e) => {
-    if (
+  if (
+    !(
       e.target === "change-avatar" ||
       e.target.closest("#change-avatar") ||
       e.target === "avatar-img" ||
-      e.target.closest("#avatar-img")
-    ) {
-      testResult = true;
-      setError(testResult, inputField, hint, label);
-      settingsWrapper.classList.remove("hidden");
-      profileSection.classList.add("hidden");
-      inputField.focus();
-    }
-
-    if (
-      !(
-        e.target === "change-avatar" ||
-        e.target.closest("#change-avatar") ||
-        e.target === "avatar-img" ||
-        e.target.closest("#avatar-img") ||
-        settingsWrapper.contains(e.target)
-      )
-    ) {
-      discard();
-    }
-  });
-}
+      e.target.closest("#avatar-img") ||
+      settingsWrapper.contains(e.target)
+    )
+  ) {
+    discard();
+  }
+});
