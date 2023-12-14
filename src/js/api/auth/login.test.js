7c361f906a4da9556ac1425eafd9d8051b8848f5
sindrebenjamin <sindskog@gmail.com>
2023-12-14 23:46:56 +0100

    create test for login.js

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -3,13 +3,19 @@
  */
 
 import '@testing-library/jest-dom';
+import 'jest-localstorage-mock';
+import fetchMock from 'jest-fetch-mock';
 
 import { login } from './login.js';
 import { load } from '../storage/load.js';
 
 document.body.innerHTML = '<button id="submit">Submit</button>';
 
+fetchMock.enableMocks();
+
 it('stores a token in local storage', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({ accessToken: 'your-token' }));
+
   const data = {
     email: 'knallis@stud.noroff.no',
     password: '12345678',
