c28fad4d36ddf87ce6d0f6a808c4023245fda5f6
sindrebenjamin <sindskog@gmail.com>
2023-12-16 00:38:09 +0100

    Automatically set end date to one week from now

--- src/js/utils/formatDate.js
+++ src/js/utils/formatDate.js
@@ -2,10 +2,10 @@ export function formatDate(inputDateString) {
   const inputDate = new Date(inputDateString);
 
   const year = inputDate.getFullYear();
-  const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
-  const day = inputDate.getDate().toString().padStart(2, "0");
-  const hours = inputDate.getHours().toString().padStart(2, "0");
-  const minutes = inputDate.getMinutes().toString().padStart(2, "0");
+  const month = (inputDate.getMonth() + 1).toString().padStart(2, '0');
+  const day = inputDate.getDate().toString().padStart(2, '0');
+  const hours = inputDate.getHours().toString().padStart(2, '0');
+  const minutes = inputDate.getMinutes().toString().padStart(2, '0');
 
   return `${year}-${month}-${day}T${hours}:${minutes}`;
 }
@@ -15,4 +15,13 @@ export function checkIfAuctionEnded(date) {
   const auctionEnd = new Date(date);
 
   return now > auctionEnd ? true : false;
+}
+
+export function oneWeekTo() {
+  const now = new Date();
+  const oneWeekLater = new Date();
+  oneWeekLater.setDate(now.getDate() + 7);
+
+  const dateFormated = formatDate(oneWeekLater);
+  return dateFormated;
 }
